492a1fce0eb38b041805fe0e3cc7e3e597b5bc34
Alejandro Almunia Herranz <aalmuniaherranz@ITEM-S21144.local>
2018-08-01 13:58:47 +0200

    Docs OK

--- index_sync.js
+++ index_sync.js
@@ -10,7 +10,11 @@ const bodyParser = require("body-parser");
 // Conectamos a la base de datos sqlite
 sqlite.connect("./database_mock.db");
 
-
+/**
+ * @description Esta función comprueba si un objeto está vacío
+ * @param {Object} obj El objeto a comprobar
+ * @returns {Boolean} Si el objeto está vacío o no
+ */
 function objectIsEmpty(obj) {
   for(var key in obj) {
       if(obj.hasOwnProperty(key))
@@ -19,17 +23,18 @@ function objectIsEmpty(obj) {
   return true;
 }
 
-
 /**
- * @returns Array El array de proyectos, desde la bbdd
+ * @description Esta función devuelve los proyectos de la base de datos
+ * @returns {Array} El array de proyectos, desde la bbdd
  */
 function getAllProjects() {
   return sqlite.run("SELECT * FROM project");
 }
 
 /**
- * @param Integer El id del proyecto a extraer
- * @returns Object El objeto de proyecto
+ * @description Esta función lee un objeto de proyecto de la base de datos
+ * @param {Integer} idProject El id del proyecto a extraer
+ * @returns {Object} El objeto de proyecto
  */
 function getSingleProject(idProject) {
   let result = sqlite.run("SELECT * FROM project WHERE id = " + idProject);
@@ -41,8 +46,9 @@ function getSingleProject(idProject) {
 }
 
 /**
- * @param Integer El id de proyecto cuyos drivers queremos
- * @returns Array Un array de objetos de tipo driver no rankeado
+ * @description Esta función  lee los drivers no rankeados (valores textuales) para un proyecto dado
+ * @param {Integer} idProject El id de proyecto cuyos drivers queremos
+ * @returns {Array} Un array de objetos de tipo driver no rankeado
  */
 function getNonRankedDriversProject(idProject) {
   let result = sqlite.run(
@@ -58,8 +64,9 @@ function getNonRankedDriversProject(idProject) {
 }
 
 /**
- * @param Integer El id de proyecto cuyos drivers queremos
- * @returns Array Un array de objetos de tipo driver rankeado
+ * @description Esta función lee los drivers rankeados para un proyecto dado
+ * @param {Integer} idProject El id de proyecto cuyos drivers queremos
+ * @returns {Array} Un array de objetos de tipo driver rankeado
  */
 function getRankedDriversProject(idProject) {
   let result = sqlite.run(
@@ -74,6 +81,13 @@ function getRankedDriversProject(idProject) {
   }
 }
 
+
+/**
+ * @description Esta función lee un driver no rankeado específico para un proyecto dado
+ * @param  {Integer} idProject El ID del proyecto
+ * @param {Integer} idDriver El ID del driver
+ * @return {Object} El objeto de driver no rankeado
+ */
 function getNonRankedDriverProject(idProject, idDriver) {
   let result = sqlite.run(
     `SELECT pj.id_driver AS id, pj.value, dr.name 
@@ -90,6 +104,12 @@ function getNonRankedDriverProject(idProject, idDriver) {
   }
 }
 
+/**
+ * @description Esta función lee un driver rankeado especifñico para un proyecto dado
+ * @param {Integer} idProject El ID del proyecto
+ * @param {Integer} idDriver El ID del driver
+ * @return {Object} El objeto de driver rankeado
+ */
 function getRankedDriverProject(idProject, idDriver) {
   let result = sqlite.run(
     `SELECT pj.id_driver AS id, pj.value, dr.name 
@@ -106,16 +126,13 @@ function getRankedDriverProject(idProject, idDriver) {
   }
 }
 
+/**
+ * @description Esta función devuelve un driver (de cualquier tipo) para un proyecto dado
+ * @param {Integer} idProject El ID del proyecto
+ * @param {Integer} idDriver El ID del driver
+ * @return {Object} El objeto de driver, sea rankeado o no
+ */
 function getDriverProject(idProject, idDriver) {
-
-  console.log(`SELECT pj.value, dr.name, dr.type FROM project_driver pj
-    INNER JOIN driver dr ON dr.id = pj.id_project
-    WHERE pj.id_project = ` +
-    idProject +
-    ` AND pj.id_driver = ` +
-    idDriver);
-
-
   let result = sqlite.run(
     `SELECT pj.value, dr.name, dr.type FROM project_driver pj
     INNER JOIN driver dr ON dr.id = pj.id_project
@@ -131,6 +148,10 @@ function getDriverProject(idProject, idDriver) {
   }
 }
 
+/**
+ * @description Esta función obtiene el ID que se debería usar para una nueva inserción en la tabla de proyectos
+ * @return {Integer|null} El nuevo ID o null si hubo un error
+ */
 function getNextProjectID() {
   let sql = `SELECT (MAX(id) + 1) AS newid FROM project`;
   let result = sqlite.run(sql);
@@ -141,12 +162,23 @@ function getNextProjectID() {
   }
 }
 
-function createProjectInDB(name) {
+/**
+ * @description Esta función crea un nuevo registro de proyecto en la base de datos
+ * @param {Object} projectData El objeto con los datos del nuevo proyecto. Debe tener campos name y bbva_uuid
+ * @return {}
+ */
+function createProjectInDB(projectData) {
+  if(!projectData.hasOwnProperty('name')) {
+    return false;
+  }
+  if(!projectData.hasOwnProperty('bbva_uuid')) {
+    return false;
+  }
   let newID = getNextProjectID();
   if(newID === null) {
     console.log('ERROR, NO HE PODIDO OBTENER EL NUEVO ID. ¿ESTÁ BIEN CONECTADA LA BASE DE DATOS?');
   } else {
-    let sql = `INSERT INTO project (id, bbva_uuid, name, completed) VALUES (`+ newID +`, 'loquesea', '` + name + `', 0)`;
+    let sql = `INSERT INTO project (id, bbva_uuid, name, completed) VALUES (`+ newID +`, '`+ projectData.bbva_uuid +`', '` + projectData.name + `', 0)`;
     try {
       let result = sqlite.run(sql);
       console.log(result);
@@ -156,7 +188,12 @@ function createProjectInDB(name) {
   }
 }
 
-
+/**
+ * @description Esta función modifica los datos de un proyecto en la base de datos
+ * @param {Object} dataModify El objeto con los datos a modificar para el proyecto
+ * @return {Boolean|null} Un true/false en función del resultado de la operación SQL,
+ * o un null si hubo algún error
+ */
 function modifyProject(dataModify) {
   let sqlUpdate = '';
   if(objectIsEmpty(dataModify)) {
@@ -195,16 +232,17 @@ app.use(function (req, res, next) {
   next();
 });
 
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
+/**
+ * Esto es para que parsée las variables de la URL como un objeto JSON, y no como texto plano.
+ * Al ser app.use(), esta operación se realiza para todas las peticiones
  */
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
+/**
+ * Esto es para que parsée el payload del cuerpo de la petición (body) como un objeto JSON, y no como texto plano.
+ * Al ser app.use(), esta operación se realiza para todas las peticiones
  */
 app.use(bodyParser.json());
 
@@ -224,7 +262,7 @@ app.get("/projects", function (request, response) {
 
 /**
  * En esta ruta se extraen todos los datos del proyecto con :id como identificador del ID
- * único del proyecto
+ * único del proyecto.
  */
 app.get("/projects/:id", function (request, response) {
   let idProject = request.params.id;
@@ -259,7 +297,10 @@ app.get("/projects_driver/:idProject/:idDriver", function (request, response) {
   response.json(aReturn);
 });
 
-app.post('/newproject', function (request, response) {
+/**
+ * Esta ruta crea un nuevo proyecto
+ */
+app.post('/project', function (request, response) {
   if (request.body.hasOwnProperty('name_project')) {
     try {
       createProjectInDB(request.body.name_project);
@@ -272,7 +313,9 @@ app.post('/newproject', function (request, response) {
   }
 });
 
-
+/**
+ * Esta ruta modifica los datos de un proyecto
+ */
 app.put('/project', function(request, response) {
   if(request.body.hasOwnProperty('project_data')) {
     if(request.body.project_data.hasOwnProperty('id')) {
@@ -292,7 +335,7 @@ app.put('/project', function(request, response) {
 
 
 /**
- * Ponemos la app a escuchar
+ * Ponemos la app a escuchar en el puerto 3003
  */
 app.listen(3003, function (request, response) {
   console.log("Planning services listening on port 3003...");
